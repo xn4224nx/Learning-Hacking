7ffe7329c8aeb8da3a80681fe847b3f33ec0d882
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-09-15 11:13:20 +0100

    Script to compile C code and create assembly dump.

---
+++ 02_7CCSONSE/1_7_Running_Example/create_obj_dump.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Compile the script
+echo "gcc -o 00_hello 00_hello.c"
+gcc -o 00_hello 00_hello.c &> /dev/null
+
+# Create the object dump in assembly
+echo "objdump -d 00_hello > 01_hello_obj_dump.txt"
+objdump -d 00_hello > 01_hello_obj_dump.txt
+
+
