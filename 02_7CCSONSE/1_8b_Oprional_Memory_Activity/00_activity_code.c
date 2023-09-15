d129f8081f477100f7cb5d996ad51e98de004da0
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-09-15 16:34:11 +0100

    Optional activity script.

---
+++ 02_7CCSONSE/1_8b_Oprional_Memory_Activity/00_activity_code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int factorial(int number);
+
+int main() {
+  
+  int x = 7;
+  printf("The factorial of %d is %d\n", x, factorial(x));
+  return 0;
+}
+
+int factorial(int number) {
+ 
+ if (number == 1) return (1);
+ else return (number * factorial(number - 1));
+}
