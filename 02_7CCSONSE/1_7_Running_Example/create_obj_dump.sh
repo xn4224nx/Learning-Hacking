f9adfe98f25c8069471ee9ffc38955331aa12e0b
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-09-15 11:15:34 +0100

    Remove the compiled file after the dump.

--- 02_7CCSONSE/1_7_Running_Example/create_obj_dump.sh
+++ 02_7CCSONSE/1_7_Running_Example/create_obj_dump.sh
@@ -8,4 +8,7 @@ gcc -o 00_hello 00_hello.c &> /dev/null
 echo "objdump -d 00_hello > 01_hello_obj_dump.txt"
 objdump -d 00_hello > 01_hello_obj_dump.txt
 
+# Delete the compile file
+echo "rm ./00_hello"
+rm ./00_hello
 
