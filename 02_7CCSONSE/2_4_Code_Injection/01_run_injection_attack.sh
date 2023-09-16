bf5964f0d4a1f1256cadcfbe3e6bc7eceb3f338f
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-09-17 00:49:42 +0100

    Piped output to file.

--- 02_7CCSONSE/2_4_Code_Injection/01_run_injection_attack.sh
+++ 02_7CCSONSE/2_4_Code_Injection/01_run_injection_attack.sh
@@ -5,6 +5,6 @@ echo -e "\nFile permissions of the files in the folder:\n"
 ls -l
 
 # Dissamble the binary file into assembly
-echo -e "\nAssemby breakdown of './00_shellcode'\n"
-ndisasm -b 32 ./00_shellcode
+echo -e "\nAssemby breakdown of './00_shellcode' printed to file."
+ndisasm -b 32 ./00_shellcode &> 02_shellcode.asm
 
