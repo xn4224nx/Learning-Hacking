84329903eb8ffb1c93d9cf36b44d0d4851de1392
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-07-11 23:30:43 +0100

    Removing log files.

--- 02_Linux_Basics_for_Hackers/11_Logging_System/00_rsyslog_daemon.sh
+++ 02_Linux_Basics_for_Hackers/11_Logging_System/00_rsyslog_daemon.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Using and configuring the RSYSLOG Logging Daemon
 
-# Show the configuration file
+# Show the log configuration file
+echo -e "\n===============\nLog Config File\n===============\n"
 cat /etc/rsyslog.conf
 
+# Show the log rotation configuration file
+echo -e "\n======================\nLog Rotate Config File\n======================\n"
+cat /etc/logrotate.conf
+
+# Securely delete the log files
+shred -f -n 10 /var/log/auth.log.*
