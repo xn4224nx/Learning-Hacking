8ba9e3887f07d837cd3a824562e49c6b66fa6636
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-07-20 23:40:53 +0100

    Wireless investigations.

---
+++ 02_Linux_Basics_for_Hackers/14_Wireless_Networks/00_identifying_networks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Basic Wireless commands
+
+# Show the activated network interfaces
+echo -e "Network Interfaces:\n"
+ifconfig
+
+# Show the Wireless interfaces
+echo -e "\nWireless interfaces:\n"
+iwconfig
+
+# Show the available wifi networks
+echo -e "\nCurrently available WiFi networks:\n"
+nmcli dev wifi
