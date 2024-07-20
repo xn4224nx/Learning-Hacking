2e735bb900428960971192ba2dc2cd1b5a00880c
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-07-20 01:20:11 +0100

    Added Metasploit integration.

--- 02_Linux_Basics_for_Hackers/12_Services/02_postgres_server.sh
+++ 02_Linux_Basics_for_Hackers/12_Services/02_postgres_server.sh
@@ -7,5 +7,17 @@ if [ "$(which postgresql)" == "" ]; then
     sudo apt install postgresql
 fi
 
+# Check if Metasploit is installed
+if [ "$(which msfconsole)" == "" ]; then
+    sudo apt update
+    sudo apt install metasploit-framework
+if
+
 # Start the service
 sudo service postgresql start
+
+# Start Metasploit
+msfconsole
+
+# Stop Postgres
+sudo service postgresql stop
