d3c9aa20c38f1d095a67d362bbfc352628916726
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-02-21 14:51:28 +0000

    Basic shell exploration.

---
+++ 04_Black_Hat_Bash/01_Bash_Basics/00_Exploring_the_Shell.sh
@@ -0,0 +1,18 @@
+# Determine the version of Bash that is present
+bash --version
+
+# Show the environmental variables
+echo -e "\nEnvironmental Variables:\n"
+env
+
+# Print specific environmental variables
+echo -e "\nSHELL = '${SHELL}'"
+echo -e "\nThe Version of Bash = '${BASH_VERSION}'"
+echo -e "\nPID of the Bash Process = '${BASHPID}'"
+echo -e "\nUsers Assigned Groups = '${GROUPS}'"
+echo -e "\nThe Current Hostname = '${HOSTNAME}'"
+echo -e "\nOperating System Type = '${OSTYPE}'"
+echo -e "\nCurrent Working Directory = '${PWD}'"
+echo -e "\nA Random Unsigned Number = '${RANDOM}'"
+echo -e "\nThe Current User ID = '${UID}'"
+echo -e "\nFull Shell Path = '${SHELL}'"
