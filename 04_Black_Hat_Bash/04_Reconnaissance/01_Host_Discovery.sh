8ab15edd590e333969a02fd74a18262af908843e
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-03-01 15:12:03 +0000

    More advanced scan techniques.

--- 04_Black_Hat_Bash/04_Reconnaissance/01_Host_Discovery.sh
+++ 04_Black_Hat_Bash/04_Reconnaissance/01_Host_Discovery.sh
@@ -3,3 +3,10 @@
 # Ping an ip address or a URL
 ping 8.8.8.8 -c 5
 ping www.bbc.co.uk  -c 5
+
+# Scan a range of IP addresses
+nmap -sn 172.16.10.0/24
+nmap -sn 172.16.10.0/24 | grep "Nmap scan" | awk -F'report for ' '{print $2}'
+
+# Scanning on the local network
+sudo arp-scan 172.16.10.0/24 -I br_public
