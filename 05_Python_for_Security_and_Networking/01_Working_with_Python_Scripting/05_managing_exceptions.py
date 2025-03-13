11939cf111f87fd019bf4c9f9dff628c1ecd22ca
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-03-13 22:07:39 +0000

    Handling errors in Python.

---
+++ 05_Python_for_Security_and_Networking/01_Working_with_Python_Scripting/05_managing_exceptions.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+# Learning and Understanding Exception Management in Python
+
+# Catching Exceptions
+try:
+    print("10/0 = f{10/0}")
+except ZeroDivisionError as err:
+    print(f"Error = {err}")
+
+try:
+    ele = [1]
+    print(ele[7])
+except Exception as err:
+    print(f"Error = {err}")
+
+
+# Show all the exceptions
+def print_exceptions(ExceptionClass, level=0):
+    if level > 1:
+        print("    |" * (level - 1), end="")
+
+    if level > 0:
+        print("    +---", end="")
+
+    print(ExceptionClass.__name__)
+
+    for subclass in ExceptionClass.__subclasses__():
+        print_exceptions(subclass, level + 1)
+
+
+print_exceptions(BaseException)
