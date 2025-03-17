e59f1a267ba13866ef09684773475b62a9e59372
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-03-17 19:58:32 +0000

    Python modules.

---
+++ 05_Python_for_Security_and_Networking/01_Working_with_Python_Scripting/06_python_modules.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+# Python Modules and Packages
+
+import sys
+
+# Show all the functions, variables and classes in the module
+print(dir(sys), end="\n\n")
+
+# Show all the built in modules
+print(sys.builtin_module_names, end="\n\n")
+
+# Show the module paths
+print(sys.path, end="\n\n")
