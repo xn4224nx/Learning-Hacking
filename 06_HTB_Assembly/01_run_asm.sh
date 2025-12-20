af0d54d44f6ee62e610fddae5ff92352009b4e41
Benjamin Mouncer <benjamin.dafydd.mouncer@gmail.com>
2025-12-20 21:43:04 +0000

    Script to run ASM files.

---
+++ 06_HTB_Assembly/01_run_asm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Remove the extention from the file
+asm_file="${1%%.*}"
+
+# Assemble the file as 64bit
+nasm -f elf64 $1
+
+# Link the file to resolve the addresses
+ld ${asm_file}".o" -o ${asm_file}
+
+# Remove the object file
+rm ${asm_file}".o"
+
+# Run the executable file or debug through gnu
+[ "$2" == "-g" ] && gdb -q ${asm_file} || ./${asm_file}
