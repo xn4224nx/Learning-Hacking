6c439630291e91bb886f12f814cd524c8a09d1dd
Benjamin Mouncer <benjamin.dafydd.mouncer@gmail.com>
2025-12-20 21:40:40 +0000

    Update 00_helloworld.asm

--- 06_HTB_Assembly/00_helloworld.asm
+++ 06_HTB_Assembly/00_helloworld.asm
@@ -1,17 +1,19 @@
-global _start
+global  _start
 
 section .data
-    message db "Hello HTB Academy!"
-    length equ $-message
-
+    message db "Hello, world!", 0x0a
+    message_len equ $-message
+    
 section .text
 _start:
     mov rax, 1
     mov rdi, 1
     mov rsi, message
-    mov rdx, length
+    mov rdx, message_len
     syscall
-
+    
     mov rax, 60
     mov rdi, 0
     syscall
+
+     
