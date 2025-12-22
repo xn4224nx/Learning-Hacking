85d4c1bde97cdfed53626577631bbadda26e3bd4
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-12-22 03:30:08 +0000

    Print introduction message.

--- 06_HTB_Assembly/02_fibonacci.asm
+++ 06_HTB_Assembly/02_fibonacci.asm
@@ -3,8 +3,18 @@
 
 global _start
 
+section .data
+    msg db "Fibonacci Sequence:", 0x0a
+
 section .text
 _start:
+    ; Print the intorduction message.
+    mov rax, 1      ; syscall number 1 - write function
+    mov rdi, 1      ; specify stout
+    mov rsi, msg    ; pass the pointer to the message to be printed
+    mov rdx, 20     ; specify the message length
+    syscall         ; call the function
+
     ; Set the two initial values of the fibonacci sequence to zero and one.
     xor rax, rax
     xor rbx, rbx
