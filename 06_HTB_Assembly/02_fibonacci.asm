e070b8e7e8d2aafbeac4b0db73d952721451483e
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-12-22 03:47:03 +0000

    Refactored to use procedures.

--- 06_HTB_Assembly/02_fibonacci.asm
+++ 06_HTB_Assembly/02_fibonacci.asm
@@ -5,17 +5,25 @@ global _start
 
 section .data
     msg db "Fibonacci Sequence:", 0x0a
+    msg_l equ $-msg
 
 section .text
 _start:
-    ; Print the intorduction message.
+    call print_intro_msg
+    call init_fib_seq
+    call fib_loop
+    call prog_exit
+
+; Print the introduction message.
+print_intro_msg:
     mov rax, 1      ; syscall number 1 - write function
     mov rdi, 1      ; specify stout
     mov rsi, msg    ; pass the pointer to the message to be printed
-    mov rdx, 20     ; specify the message length
+    mov rdx, msg_l  ; specify the message length
     syscall         ; call the function
 
-    ; Set the two initial values of the fibonacci sequence to zero and one.
+; Set the two initial values of the fibonacci sequence to zero and one.
+init_fib_seq:
     xor rax, rax
     xor rbx, rbx
     inc rbx
@@ -23,12 +31,15 @@ _start:
     ; Define how many fibonnaci values to generate.
     mov rcx, 10
 
-fibloop:
+fib_loop:
    add rax, rbx     ; Generate the next number
    xchg rax, rbx    ; Swap the two numbers
-   loop fibloop
+   loop fib_loop
+
+
 
    ; Close down the program gracefully.
+prog_exit:
    mov rax, 60
    mov rdi, 0
    syscall
