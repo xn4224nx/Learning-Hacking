589ec1695946768f9303a25692190752c64621a7
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-12-25 08:43:47 +0000

    Added shellcode to launch shell

--- 06_HTB_Assembly/04_running_shellcode.py
+++ 06_HTB_Assembly/04_running_shellcode.py
@@ -5,9 +5,7 @@ from pwn import *
 context(os="Linux", arch="amd64", log_level="error")
 
 sh_code = (
-    "4831db66bb79215348bb422041636164656d5"
-    "348bb48656c6c6f204854534889e64831c0b0"
-    "014831ff40b7014831d2b2120f054831c0043c4030ff0f05"
+    "b03b4831d25248bf2f62696e2f2f7368574889e752574889e60f05"
 )
 
 run_shellcode(unhex(sh_code)).interactive()
