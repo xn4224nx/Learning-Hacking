2d74ecb3a5522198a3373ca2dcbab185f0d30636
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-12-28 23:49:31 +0000

    Mutable Shared Pointer.

--- 07_Black_Hat_Rust/chapter_01/02_smart_pointers/src/main.rs
+++ 07_Black_Hat_Rust/chapter_01/02_smart_pointers/src/main.rs
@@ -2,16 +2,25 @@
  * Smart Pointers
  */
 
+use std::cell::{RefCell, RefMut};
 use std::rc::Rc;
 
 fn main() {
     let sm_pntr0 = Rc::new(1);
+    let shared_msg = Rc::new(RefCell::new("Hello".to_string()));
 
-    /* Access the first pointer in a differnt scope. */
     {
+        /* Modify the shared message. */
+        let mut modify_msg: RefMut<String> = shared_msg.borrow_mut();
+        modify_msg.push_str(" World!");
+
+        /* Access the first pointer in a different scope. */
         let sm_pntr1 = Rc::clone(&sm_pntr0);
         println!("{}", *sm_pntr1);
     }
 
     println!("{}", *sm_pntr0);
+
+    /* Show the modified string. */
+    println!("{}", shared_msg.take());
 }
