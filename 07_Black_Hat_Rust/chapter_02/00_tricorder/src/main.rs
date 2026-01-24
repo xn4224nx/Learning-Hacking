80964f17f2b60f996483fea32fab61a9448f466b
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-01-24 11:27:35 +0000

    Converting the text to a JSON.

--- 07_Black_Hat_Rust/chapter_02/00_tricorder/src/main.rs
+++ 07_Black_Hat_Rust/chapter_02/00_tricorder/src/main.rs
@@ -11,5 +11,9 @@ fn main() {
         .text()
         .expect("Request failed to get a response!");
 
-    println!("{}", req_body);
+    /* Parse the raw text into a structured form. */
+    let dom_info: serde_json::Value =
+        serde_json::from_str(&req_body).expect("Request text could not be parsed.");
+
+    println!("{:?}", dom_info);
 }
