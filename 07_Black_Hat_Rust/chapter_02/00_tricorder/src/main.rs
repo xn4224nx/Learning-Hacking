0df36e404fffcec9ef8f540cf75368cc2fd6b687
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-01-24 13:46:21 +0000

    Moved domain extraction to a function.

--- 07_Black_Hat_Rust/chapter_02/00_tricorder/src/main.rs
+++ 07_Black_Hat_Rust/chapter_02/00_tricorder/src/main.rs
@@ -5,7 +5,6 @@
 use std::collections::HashSet;
 
 fn main() {
-    let mut sub_domains = HashSet::new();
     let scrape_url = String::from("https://crt.sh/?q=%25.bbc.co.uk&output=json");
 
     /* Get the raw request from the site. */
@@ -14,25 +13,35 @@ fn main() {
         .text()
         .expect("Request failed to get a response!");
 
-    /* Parse the raw text into a structured form. */
-    let all_dom_info: serde_json::Value =
-        serde_json::from_str(&req_body).expect("Request text could not be parsed.");
+    println!("{:?}", extract_subdomains(&req_body).unwrap());
+}
+
+/// Take the raw body result and extract the subdomains
+fn extract_subdomains(raw_body: &String) -> Option<Vec<String>> {
+    let mut sub_domains = HashSet::new();
+
+    /* Parse the raw text into a Json. */
+    let all_dom_info: serde_json::Value = serde_json::from_str(raw_body).ok()?;
 
     /* From the parsed data extract the subdomains. */
-    for dom_info in all_dom_info.as_array().unwrap().into_iter() {
+    for dom_info in all_dom_info.as_array()?.into_iter() {
         for pote_key_nm in vec!["common_name", "name_value"] {
             let Some(s_dom) = dom_info.get(pote_key_nm).and_then(|x| x.as_str()) else {
                 continue;
             };
 
-            /* Ignore wild card domains */
-            if s_dom.contains("*") {
+            /* Ignore wild card and inalid domains */
+            if s_dom.contains("*") || s_dom.contains("\n") {
                 continue;
             }
-
-            sub_domains.insert(s_dom.to_string());
+            sub_domains.insert(s_dom.trim().to_string());
         }
     }
 
-    println!("{:?}", sub_domains);
+    /* Ensure sub-domains have been found. */
+    return if sub_domains.is_empty() {
+        None
+    } else {
+        Some(sub_domains.into_iter().collect())
+    };
 }
