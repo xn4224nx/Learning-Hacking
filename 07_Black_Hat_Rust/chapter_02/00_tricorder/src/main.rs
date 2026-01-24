4220a528bb4e0d050ac6347cb95c852340e0cb42
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-01-24 13:03:48 +0000

    Extract and de-duplicate the subdomains.

--- 07_Black_Hat_Rust/chapter_02/00_tricorder/src/main.rs
+++ 07_Black_Hat_Rust/chapter_02/00_tricorder/src/main.rs
@@ -2,7 +2,10 @@
  * Chapter 2 - Tricorder
  */
 
+use std::collections::HashSet;
+
 fn main() {
+    let mut sub_domains = HashSet::new();
     let scrape_url = String::from("https://crt.sh/?q=%25.bbc.co.uk&output=json");
 
     /* Get the raw request from the site. */
@@ -12,8 +15,24 @@ fn main() {
         .expect("Request failed to get a response!");
 
     /* Parse the raw text into a structured form. */
-    let dom_info: serde_json::Value =
+    let all_dom_info: serde_json::Value =
         serde_json::from_str(&req_body).expect("Request text could not be parsed.");
 
-    println!("{:?}", dom_info);
+    /* From the parsed data extract the subdomains. */
+    for dom_info in all_dom_info.as_array().unwrap().into_iter() {
+        for pote_key_nm in vec!["common_name", "name_value"] {
+            let Some(s_dom) = dom_info.get(pote_key_nm).and_then(|x| x.as_str()) else {
+                continue;
+            };
+
+            /* Ignore wild card domains */
+            if s_dom.contains("*") {
+                continue;
+            }
+
+            sub_domains.insert(s_dom.to_string());
+        }
+    }
+
+    println!("{:?}", sub_domains);
 }
