6fb5d14a99ba75180578e00a9366fcb813d505a1
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-01-24 10:45:48 +0000

    Basic scrape.

---
+++ 07_Black_Hat_Rust/chapter_02/00_tricorder/src/main.rs
@@ -0,0 +1,15 @@
+/*
+ * Chapter 2 - Tricorder
+ */
+
+fn main() {
+    let scrape_url = String::from("https://crt.sh/?q=%25.bbc.co.uk&output=json");
+
+    /* Get the raw request from the site. */
+    let req_result = reqwest::blocking::get(scrape_url).expect("Request failed to get a response!");
+    let req_body = req_result
+        .text()
+        .expect("Request failed to get a response!");
+
+    println!("{}", req_body);
+}
