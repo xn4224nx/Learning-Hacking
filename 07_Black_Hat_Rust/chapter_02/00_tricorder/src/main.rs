7b1c7f669d609d36ada64699e07531a801f5ab73
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2026-02-11 01:25:39 +0000

    Crude domain search.

--- 07_Black_Hat_Rust/chapter_02/00_tricorder/src/main.rs
+++ 07_Black_Hat_Rust/chapter_02/00_tricorder/src/main.rs
@@ -6,7 +6,19 @@ mod parse_args;
 mod scanner;
 mod sub_domain_lookup;
 
+const RETRY_NUM: usize = 5;
+
 fn main() {
     let root_domain = parse_args::get();
-    println!("{:?}", sub_domain_lookup::get(&root_domain).unwrap());
+
+    println!("\nSub-domain Search\n=================");
+
+    /* Consult the API and try and get the related sub-domains. */
+    for atm_idx in 0..RETRY_NUM {
+        let Some(sdom) = sub_domain_lookup::get(&root_domain) else {
+            println!("\tAttempt {} Failed.", atm_idx + 1);
+            continue;
+        };
+        println!("\tAPI Search Complete: {} URLs found.\n", sdom.len());
+    }
 }
